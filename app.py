16e39cc7951ac13a53926c8d83f00482886597b3
ayure27-max <ayure27@gmail.com>
2026-01-30 17:04:24 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -133,5 +133,4 @@ def reply_message(reply_token, text):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
