cdf0e1d73ffccf61b8665c1ac2e806e76f350e6e
ayure27-max <ayure27@gmail.com>
2026-01-31 23:28:56 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ def send_reply(reply_token, text, quick_reply=None):
         "messages": [{"type": "text", "text": text}]
     }
     if quick_reply:
-    message["quickReply"] = {"items": quick_reply}
+        message["quickReply"] = {"items": quick_reply}
         
-    data = {
-    "replyToken": reply_token,
-    "messages": [message]
-    }
+        data = {
+        "replyToken": reply_token,
+        "messages": [message]
+        }
         
     requests.post(url, headers=headers, json=data)
 
