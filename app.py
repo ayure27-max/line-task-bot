4a705ddc51d7dfab26ab773decb15da7a409f6c2
ayure27-max <ayure27@gmail.com>
2026-02-07 22:24:36 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 RICHMENU_ID = "richmenu-2cfad97cc5a5d06f6419a14d75e7777a"
 IMAGE_PATH = "richmenu.png"
 
-url = f"https://api.line.me/v2/bot/richmenu/{RICHMENU_ID}/content"
+url = "https://api.line.me/v2/bot/richmenu"
 
 headers = {
     "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
