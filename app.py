15e443e55e98b73de9152246331688b3a729b5a2
ayure27-max <ayure27@gmail.com>
2026-02-07 22:02:40 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import Flask, request
-import os
 import requests
-import json
-
-app = Flask(__name__)
+import os
 
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-print("TOKEN:", LINE_CHANNEL_ACCESS_TOKEN)
-RICHMENU_ID ='richmenu-2cfad97cc5a5d06f6419a14d75e7777a'}
-IMAGE_PATH =
-TASK_FILE = "tasks.json"
+RICHMENU_ID = "richmenu-2cfad97cc5a5d06f6419a14d75e7777a"
+IMAGE_PATH = "richmenu.png"
 
-url = "https://api.line.me/v2/bot/richmenu"
+url = f"https://api.line.me/v2/bot/richmenu/{RICHMENU_ID}/content"
 
 headers = {
     "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
     "Content-Type": "image/png"
 }
 
+with open(IMAGE_PATH, "rb") as f:
+    r = requests.post(url, headers=headers, data=f)
+
+print("status:", r.status_code)
+print("response:", r.text)
+
 richmenu = {
     "size": {
-        "width": 2500,
-        "height": 1686
+        "width": 1040,
+        "height": 1040
     },
     "selected": True,
     "name": "main-menu",
